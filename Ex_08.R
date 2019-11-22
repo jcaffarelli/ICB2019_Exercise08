c6ede9607e62c6cf77ee1db3e13590b2d391bacb
John Caffarelli <jcaffare@nd.edu>
2019-11-21 22:26:11 -0500

    for loop for UW scores

--- Ex_08.R
+++ Ex_08.R
@@ -3,3 +3,28 @@ setwd("C:/Users/jcaff/Documents/Notre Dame/Senior/fall_19/Biocomputing/R/Tutoria
 
 #clear global environment
 rm(list = ls())
+
+#loading Score Data
+scoreUWMSU<-read.table("UWvMSU_1-22-13.txt",header = TRUE,sep = "")
+
+#separate scores by team into separate data tables
+scoreUW<-scoreUWMSU[scoreUWMSU$team=="UW",]
+scoreMSU<-scoreUWMSU[scoreUWMSU$team=="MSU",]
+
+#check final (sum) of scores as a check for later!
+sum(scoreMSU$score)#49
+sum(scoreUW$score)#47
+#close game!
+
+#Need to create for-loops for each team that take cumulative score at each time point
+#set initial scores to 0
+cumScoreUW=0
+cumScoreMSU=0
+
+#UW for-loop to generate cumulative score
+for (i in 1:nrow(scoreUW)){
+  cumScoreUW<-cumScoreUW+scoreUW$score[i] #this line generates cumulative sum
+  scoreUW$score[i]=cumScoreUW #replaces scores with cumulative score
+}
+
+
