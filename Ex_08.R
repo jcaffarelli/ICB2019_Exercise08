ed1cbca305d6542a38c89eef90910e4d12ac4e50
John Caffarelli <jcaffare@nd.edu>
2019-11-21 22:07:59 -0500

    loading data and creating score files

---
+++ Ex_08.R
@@ -0,0 +1,5 @@
+#setting working directory for Exercise 08
+setwd("C:/Users/jcaff/Documents/Notre Dame/Senior/fall_19/Biocomputing/R/Tutorial/Ex_08/ICB2019_Exercise08")
+
+#clear global environment
+rm(list = ls())
