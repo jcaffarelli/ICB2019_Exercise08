880cd0244262fd9a34d1a75a573a79b0600ad18b
John Caffarelli <jcaffare@nd.edu>
2019-11-21 22:40:26 -0500

    updating with question 1 complete

--- Ex_08.R
+++ Ex_08.R
@@ -4,6 +4,8 @@ setwd("C:/Users/jcaff/Documents/Notre Dame/Senior/fall_19/Biocomputing/R/Tutoria
 #clear global environment
 rm(list = ls())
 
+###  QUESTION 1  ###
+
 #loading Score Data
 scoreUWMSU<-read.table("UWvMSU_1-22-13.txt",header = TRUE,sep = "")
 
@@ -26,13 +28,23 @@ for (i in 1:nrow(scoreUW)){
   cumScoreUW<-cumScoreUW+scoreUW$score[i] #this line generates cumulative sum
   scoreUW$score[i]=cumScoreUW #replaces scores with cumulative score
 }
-
-#final UW score is 50, need to see what error might be
+#final UW score is orrect at 47
 
 #MSU for-loop to generate cumulative score
 for (i in 1:nrow(scoreMSU)){
   cumScoreMSU<-cumScoreMSU+scoreMSU$score[i]
   scoreMSU$score[i]=cumScoreMSU
 }
-
 #final MSU score is correct at 49
+
+#generating graph!
+#setting axes
+x=scoreUW$time
+y=scoreUW$score
+#creating graph with UW Data
+plot(x,y,"l",col="red",xlab = "Time",ylab = "Score",main = "UW-MSU ScoreTracker")
+#adding MSU Data
+x2=scoreMSU$time
+y2=scoreMSU$score
+#adding line for MSU
+lines(x2,y2,col="dark green")
